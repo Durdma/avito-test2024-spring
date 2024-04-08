83351a2304f0540ede99277f9cc73252d31a834b
Durdma <glodiolusmmm@gmail.com>
2024-04-09 00:56:14 +0300

    added custom error for json; implemented logic for tags;

--- internal/controller/httpv1/tags_features.go
+++ internal/controller/httpv1/tags_features.go
@@ -1,8 +1,11 @@
 package httpv1
 
 import (
+	"errors"
 	"github.com/gin-gonic/gin"
 	"net/http"
+	"strconv"
+	"strings"
 )
 
 func (h *Handler) initTagsFeaturesRoutes(api *gin.RouterGroup) {
@@ -24,16 +27,97 @@ func (h *Handler) initTagsFeaturesRoutes(api *gin.RouterGroup) {
 func (h *Handler) addTag(ctx *gin.Context) {
 	err := h.tagsService.AddTag(ctx)
 	if err != nil {
-		ctx.AbortWithStatus(http.StatusInternalServerError) // TODO add custom error
+		h.logger.Error().Err(err).
+			Str("method", ctx.Request.Method).
+			Str("url", ctx.Request.RequestURI).
+			Int("status_code", http.StatusInternalServerError).Msg("")
+		newErrorResponse(ctx, http.StatusInternalServerError, err.Error())
 		return
 	}
 
-	ctx.AbortWithStatus(http.StatusCreated)
+	ctx.Status(http.StatusCreated)
 }
 
-func (h *Handler) deleteTag(ctx *gin.Context) {}
+func (h *Handler) deleteTag(ctx *gin.Context) {
+	tagId, err := strconv.Atoi(ctx.Param("id"))
+	if err != nil {
+		h.logger.Error().Err(err).
+			Str("method", ctx.Request.Method).
+			Str("url", ctx.Request.RequestURI).
+			Int("status_code", http.StatusBadRequest).
+			Msg("invalid id format")
+		newErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
 
-func (h *Handler) getAllTags(ctx *gin.Context) {}
+	err = h.tagsService.DeleteTag(ctx, tagId)
+	if err != nil {
+		if strings.Contains(err.Error(), "not found") {
+			h.logger.Error().Err(err).
+				Str("method", ctx.Request.Method).
+				Str("url", ctx.Request.RequestURI).
+				Int("status_code", http.StatusNotFound).
+				Msg("")
+			newErrorResponse(ctx, http.StatusNotFound, err.Error())
+			return
+		}
+
+		h.logger.Error().Err(err).
+			Str("method", ctx.Request.Method).
+			Str("url", ctx.Request.RequestURI).
+			Int("status_code", http.StatusInternalServerError).
+			Msg("")
+		newErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.Status(http.StatusOK)
+}
+
+func (h *Handler) getAllTags(ctx *gin.Context) {
+	limit, err := strconv.Atoi(ctx.Query("limit"))
+	if err != nil && errors.Is(err, strconv.ErrSyntax) && ctx.Query("limit") != "" {
+		h.logger.Error().Err(err).
+			Str("method", ctx.Request.Method).
+			Str("url", ctx.Request.RequestURI).
+			Int("status_code", http.StatusBadRequest).
+			Msg("invalid limit format")
+		newErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if ctx.Query("limit") == "" {
+		limit = 0
+	}
+
+	offset, err := strconv.Atoi(ctx.Query("offset"))
+	if err != nil && errors.Is(err, strconv.ErrSyntax) && ctx.Query("offset") != "" {
+		h.logger.Error().Err(err).
+			Str("method", ctx.Request.Method).
+			Str("url", ctx.Request.RequestURI).
+			Int("status_code", http.StatusBadRequest).
+			Msg("invalid offset format")
+		newErrorResponse(ctx, http.StatusBadRequest, err.Error())
+		return
+	}
+
+	if ctx.Query("offset") == "" {
+		offset = 0
+	}
+
+	tags, err := h.tagsService.GetAllTags(ctx, limit, offset)
+	if err != nil {
+		h.logger.Error().Err(err).
+			Str("method", ctx.Request.Method).
+			Str("url", ctx.Request.RequestURI).
+			Int("status_code", http.StatusInternalServerError).
+			Msg("")
+		newErrorResponse(ctx, http.StatusInternalServerError, err.Error())
+		return
+	}
+
+	ctx.JSON(http.StatusOK, tags)
+}
 
 func (h *Handler) addFeature(ctx *gin.Context) {}
 
